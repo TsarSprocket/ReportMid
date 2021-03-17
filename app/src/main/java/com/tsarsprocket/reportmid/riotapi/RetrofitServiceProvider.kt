b5b265d55bbe8b30f5528a13cf2c89073edd9140
TsarSprocket <Michael.Tsarev@gmail.com>
2021-03-17 18:39:54 +0300

    Request rate policy implemented

--- app/src/main/java/com/tsarsprocket/reportmid/riotapi/RetrofitServiceProvider.kt
+++ app/src/main/java/com/tsarsprocket/reportmid/riotapi/RetrofitServiceProvider.kt
@@ -5,26 +5,30 @@ import com.tsarsprocket.reportmid.R
 import com.tsarsprocket.reportmid.RIOT_APY_KEY_HEADER
 import com.tsarsprocket.reportmid.model.RegionModel
 import com.tsarsprocket.reportmid.model.ServerModel
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.InputStreamReader
 import java.util.*
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.collections.HashMap
 
 @Singleton
 class RetrofitServiceProvider @Inject constructor(val context: Context) {
-    private val okClient = OkHttpClient.Builder().addInterceptor { chain ->
-        chain.proceed(
-            chain.request().newBuilder()
-                .header(RIOT_APY_KEY_HEADER, InputStreamReader(context.resources.openRawResource(R.raw.riot_api_key)).readText())
-                .build()
-        )
-    }.build()
+    private val okClient = OkHttpClient.Builder()
+        .addInterceptor(RequestRatePolicy(20, 1, TimeUnit.SECONDS))
+        .addInterceptor(RequestRatePolicy(100, 2, TimeUnit.MINUTES))
+        .addInterceptor { chain ->
+            chain.proceed(
+                chain.request().newBuilder()
+                    .header(RIOT_APY_KEY_HEADER, InputStreamReader(context.resources.openRawResource(R.raw.riot_api_key)).readText())
+                    .build()
+            )
+        }
+        .build()
     private val adapterFactory = RxJava2CallAdapterFactory.create()
     private val converterFactory = GsonConverterFactory.create()
     private val retroCache = Collections.synchronizedMap(HashMap<ServerModel, RetroWithServices>())
