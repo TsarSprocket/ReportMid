ce975ef2d59500d30205a172d0b9f511cc294a15
TsarSprocket <Michael.Tsarev@gmail.com>
2025-04-26 22:40:52 +0300

    Fixed coroutine issue in Request Manager + minor clean-up

--- requestManagerImpl/src/main/java/com/tsarsprocket/reportmid/requestManagerImpl/data/RequestManagerImpl.kt
+++ requestManagerImpl/src/main/java/com/tsarsprocket/reportmid/requestManagerImpl/data/RequestManagerImpl.kt
@@ -1,10 +1,13 @@
 package com.tsarsprocket.reportmid.requestManagerImpl.data
 
 import com.tsarsprocket.reportmid.baseApi.di.qualifiers.Io
+import com.tsarsprocket.reportmid.requestManagerApi.data.CancelableRequest
+import com.tsarsprocket.reportmid.requestManagerApi.data.Request
+import com.tsarsprocket.reportmid.requestManagerApi.data.RequestKey
+import com.tsarsprocket.reportmid.requestManagerApi.data.RequestResult
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
@@ -15,40 +18,33 @@ internal class RequestManagerImpl @Inject constructor(
     @Io val ioDispatcher: CoroutineDispatcher,
 ) : com.tsarsprocket.reportmid.requestManagerApi.data.RequestManager {
 
-    private val requestMap: ConcurrentHashMap<com.tsarsprocket.reportmid.requestManagerApi.data.RequestKey, RequestInfo> = ConcurrentHashMap()
+    private val requestMap: ConcurrentHashMap<RequestKey, RequestInfo> = ConcurrentHashMap()
 
-    override suspend fun <R : com.tsarsprocket.reportmid.requestManagerApi.data.RequestResult> request(
-        request: com.tsarsprocket.reportmid.requestManagerApi.data.Request<*, R>,
+    override suspend fun <R : RequestResult> request(
+        request: Request<*, R>,
         clazz: KClass<R>
-    ): R {
-
-        suspend fun createNewEntry(request: com.tsarsprocket.reportmid.requestManagerApi.data.Request<*, R>): com.tsarsprocket.reportmid.requestManagerApi.data.RequestResult {
-            return RequestInfo(request, coroutineScope { async { request().also { requestMap.remove(request.key) } } }).run {
-                requestMap[request.key] = this
-                deferredValue.await()
-            }
-        }
-
-        return withContext(ioDispatcher) {
-            clazz.cast(requestMap[request.key]?.run {
-                deferredValue.await()
-            } ?: createNewEntry(request))
-        }
+    ): R = withContext(ioDispatcher) {
+        clazz.cast(requestMap[request.key]?.run {
+            deferredValue.await()
+        } ?: RequestInfo(request, async { request().also { requestMap.remove(request.key) } }).run {
+            requestMap[request.key] = this
+            deferredValue.await()
+        })
     }
 
-    override fun cancelRequest(key: com.tsarsprocket.reportmid.requestManagerApi.data.RequestKey) {
+    override fun cancelRequest(key: RequestKey) {
         requestMap[key]?.let { requestInfo ->
-            if(requestInfo.request is com.tsarsprocket.reportmid.requestManagerApi.data.CancelableRequest) requestInfo.request.cancel()
+            if(requestInfo.request is CancelableRequest) requestInfo.request.cancel()
             requestMap.remove(key)
         }
     }
 
-    override fun getRequest(key: com.tsarsprocket.reportmid.requestManagerApi.data.RequestKey): com.tsarsprocket.reportmid.requestManagerApi.data.Request<*, *>? = requestMap[key]?.request
+    override fun getRequest(key: RequestKey): Request<*, *>? = requestMap[key]?.request
 
-    override fun hasRequest(key: com.tsarsprocket.reportmid.requestManagerApi.data.RequestKey): Boolean = requestMap.contains(key)
+    override fun hasRequest(key: RequestKey): Boolean = requestMap.contains(key)
 
     private data class RequestInfo(
-        val request: com.tsarsprocket.reportmid.requestManagerApi.data.Request<*, *>,
-        val deferredValue: Deferred<com.tsarsprocket.reportmid.requestManagerApi.data.RequestResult>,
+        val request: Request<*, *>,
+        val deferredValue: Deferred<RequestResult>,
     )
 }
